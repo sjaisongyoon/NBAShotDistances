e6bd64179fb2fce94bba906ef718878325b274be
Sam Yoon <sjaisongyoon@gmail.com>
2019-07-30 23:20:02 -0700

    Refactor update pattern. Add play, pause, reset button

--- src/bubble_chart_nba.js
+++ src/bubble_chart_nba.js
@@ -1,9 +1,8 @@
 import * as d3 from "d3"
 import d3Tip from 'd3-tip';
 import { compareBy } from './format_data';
-// (d3 as any).tip = d3Tip;
-// import {hide, show} from 'd3-tip';
-// 
+import $ from "jquery";
+
 const margin = { top: 100, right: 200, bottom: 300, left: 50 };
 const width = 1200 - margin.left - margin.right;
 const height = 1200 - margin.top - margin.bottom;
@@ -14,6 +13,8 @@ export const madeShotDist = "Avg. Made Shot Dis.(ft.)"
 export const missedShotDist = "Avg. Missed Shot Dis.(ft.)"
 
 let time = 0;
+let interval;
+let formattedData;
 const svg = d3.select('#chart-area')
     .append('svg')
     .attr("width", width + margin.left + margin.right)
@@ -21,13 +22,6 @@ const svg = d3.select('#chart-area')
 
 const g = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`)
-
-// const tip = d3Tip().attr('class', 'd3-tip')
-//     .html(d => {
-//         let text = "<strong>Team:<strong> <span style='colored:red'>" + d.team + "</span><br>"
-//         text += "<strong>Shot Att. Avg. Dist. (ft.):<strong> <span style='colored:red'>" + d.team + "</span><br>"
-//     });
-// g.call(tip);
 
 const tip = d3Tip().attr('class', 'd3-tip')
     .html(d => {
@@ -64,14 +58,6 @@ const rightAxisGroup = g.append("g")
 const xAxisGroup = g.append("g")
     .attr("class", "x-axis")
 
-// g.append("text")
-//     .attr("class", "x-axis-label")
-//     .attr("y", width / 2)
-//     .attr("x", height + 200)
-//     .attr("font-size", "20px")
-//     .attr("text-anchor", "middle")
-//     .text("Team")
-
 g.append("ellipse")
     .attr("cx", 0)
     .attr("cy", height / 2)
@@ -95,15 +81,16 @@ let timeLabel = g.append("text")
     .attr("y", height/10)
     .attr("x", 30);
 
-// d3tip tip = d3.tip().attr('class', 'd3tip').html(function(d) {return d;}), with 2 event listeners
 // on method
 console.log("version1")
+let format = [];
+let filtered;
+
 d3.json("dist/data/team_shot_data.json").then(function (data) {
 
     // console.log("version1")
     data.forEach(datum => datum.Season = +datum.Season.split("-")[1])
     data.reverse();
-    let format = [];
     for (let i = 0; i < data.length; i++) {
         const datum = data[i];
         if (!format.length || datum.Season !== format[format.length - 1].season) {
@@ -115,7 +102,7 @@ d3.json("dist/data/team_shot_data.json").then(function (data) {
             format[format.length - 1].teams.push(datum)
         }
     }
-    let filtered = format.map(seasonData => {
+    filtered = format.map(seasonData => {
         return seasonData.teams.filter(team => {
             return Object.values(team).every(value => value[attShotDist] !== null)
         })
@@ -123,22 +110,41 @@ d3.json("dist/data/team_shot_data.json").then(function (data) {
 
     x.domain([0, 16]);
 
-    d3.interval(() => {
-        time = (time < format.length - 1) ? time + 1 : 0
-        update(filtered[time]);
-
-    }, 1000);
     update(filtered[0])
 
 })
 
+function step(){
+    time = (time < format.length - 1) ? time + 1 : 0
+    update(filtered[time]);
+}
+
+$("#play-button")
+    .on("click", function(){
+        let button = $(this);
+        if (button.text() == "Play"){
+            button.text("Pause");
+            interval = setInterval(step, 1000);   
+        } else {
+            button.text("Play");
+            clearInterval(interval);
+        }
+    })
+
+$("#reset-button")
+    .on("click", function(){
+        time = 0;
+        return update(filtered[0]);
+    })
+
+    
 function update(data) {
 
     const teamColor = d3.scaleSequential(d3.interpolateOrRd);
-    // const t = d3.transition()
-    //     .duration(200)
-    //     .ease(d3.easeLinear)
-    //     // .delay();
+    const t = d3.transition()
+        .duration(200)
+        .ease(d3.easeLinear)
+        // .delay();
     y.domain(data.map(d => d[team]))
 
     const t2 = d3.transition()
@@ -163,32 +169,27 @@ function update(data) {
             // debugger
             return d
         })
-        // debugger;
-        // joins.exit()
-        //     // .transition(t)
-        //     .remove()
 
-        joins.enter()
-            .append("line")
-            .attr("stroke", "grey")
-            .attr("stroke-width", 3)
-            // .attr("y2", height / 2)
-            // .attr("x2", 0)
-            // .attr("x1", 0)
-            .attr("x1", (d) => x(d[attShotDist]))
-            .attr("y1", d => {
-                return y(d[team]) + y.bandwidth() / 2
-            })
-            .attr("y2", height/2)
-            .attr("x2", 0)
-            .merge(joins)
-            // .transition(t2)
-            .attr("x1", (d) => x(d[attShotDist]))
-            .attr("y1", d => {
-                return y(d[team]) + y.bandwidth() / 2
-            })
-            .attr("y2", height/2)
-            .attr("x2", 0)
+    joins.enter()
+        .append("line")
+        .attr("stroke", "grey")
+        .attr("stroke-width", 3)
+        .attr("x1", (d) => x(d[attShotDist]))
+        .attr("y1", d => {
+            return y(d[team]) + y.bandwidth() / 2
+        })
+        .attr("y2", height/2)
+        .attr("x2", 0)
+        .merge(joins)
+        .transition(t)
+        .attr("x1", (d) => x(d[attShotDist]))
+        .attr("y1", d => {
+            return y(d[team]) + y.bandwidth() / 2
+        })
+        .attr("y2", height/2)
+        .attr("x2", 0)
+        .style("z-index", 99)
+        .style("position", "relative")
 
     const teams = g.selectAll("circle")
         .data(data, function (d) {
@@ -196,34 +197,37 @@ function update(data) {
         })
 
     //EXIT
-    // debugger
     teams.exit()
         .attr("class", "exit")
-        // .transition(t)
+        .transition(t)
         .attr("height", 0)
         .remove();
+
     joins.exit()
         // .transition(t)
-        // .transition(t2)
         .remove()
 
+
     //ENTER
-    // debugger;
     teams.enter()
         .append("circle")
-        .attr("class", "enter")
+        .attr("class", "enter teamcircle")
         .attr("id", (d,i)=> d[team])
         .attr("fill", (d, i) => teamColor(i / (data.length)))
         .on("mouseover", tip.show)
         .on("mouseout", tip.hide)
         .merge(teams)
-        // .transition(t)
+        .transition(t)
         .attr("cy", d => y(d[team]) + y.bandwidth() / 2)
         .attr("cx", d => x(d[attShotDist]))
         .attr("r", d => {
             // debugger
             return Math.sqrt(area(d[madeShotDist]) / Math.PI)
         })
+        .style("z-index", 999)
+        .style("position", "relative")
+
+
     
     timeLabel.text("Season: " + `${+(time + 1997 -1)}` + " - " +(time + 1997) )
 
